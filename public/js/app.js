0fe17b5896737eab4c97d3b5a3cf86ca5e3b8595
RC Maples <rcmaples@users.noreply.github.com>
2018-09-19 20:49:10 -0400

    prepping for client work.

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,89 @@
+/*
+app.js
+  will have access to
+    - global jQuery;
+    - handle
+    - render
+
+  will
+    - listen for events ('click', 'submit')
+    - call handler methods base on event
+    - utilize and pass STORE/STATE
+    - be able to change STORE
+
+  will not
+    - directly manipulate DOM
+    - directly make HTTP calls to api
+
+Notes:
+Storing STORE globaly to allow acces to dev console
+STORE will be passed between handle and render to control
+what's shown
+
+jQuery concepts:
+  $('SELECTOR').on('ACTION', 'SUBSELECT', DATA, FUNCTION/METHOD)
+
+  When ACTION is performed on SELECTOR[subselect],
+            pass DATA into the FUNCTION/METHOD
+
+*/
+
 'use strict';
+let STORE = {};
 
 function jinkies() {
-  $('#jq-dropdown-id-1')
-    .on('show', function(event, dropdownData) {
-      console.log(dropdownData);
-    })
-    .on('hide', function(event, dropdownData) {
-      console.log(dropdownData);
-    });
-}
+  STORE = {
+    dev: true,
+    view: 'welcome',
+    backTo: null,
+    list: [],
+    item: {},
+    token: localStorage.getItem('token'),
+    emailAddress: null,
+    timer: {
+      status: null,
+      warning: 30000,
+      remaining: null,
+      polling: 60000
+    }
+  };
 
-$('.collapse').on('click', function(event, data) {
-  $('.slide').slideToggle();
-});
+  if (localStorage.getItem('token')) {
+    handle.preAuth(localStorage.getItem('token'));
+  } else {
+    render.page(STORE);
+  }
 
+  // SIGNUP, SIGNIN
+  $('#welcome').on('click', '.js-signup', STORE, handle.newSignup);
+  $('#signup').on('submit', STORE, handle.signup);
+  $('#signup').on('click', '.signinlink', STORE, handle.welcomeSignIn);
+
+  $('#welcome').on('click', '.js-signin', STORE, handle.welcomeSignIn);
+  $('#signin').on('submit', STORE, handle.signin);
+  $('.signinlink').on('click', STORE, handle.signin);
+  $('#confirmation').on('submit', STORE, handle.welcomeSignIn);
+
+  // LIST
+  $('#viewForm').on('click', handle.viewForm);
+  $('#newItemForm').on('submit', STORE, handle.create);
+  $('ul').on('click', 'li', STORE, handle.complete);
+
+  // LIST MODIFICATION
+
+  // SIGNOUT
+  $('#signoutButton').on('click', STORE, handle.signout);
+
+  // REFRESH TOKEN
+  // If you're actively clicking on the page, refresh token expiry
+  // $("body").on("click", STORE, handle.refresh);
+
+  // call checkExpiry on doc ready.
+  // handle.checkExpiry(STORE);
+
+  // poll to check expiry based on STORE.timer.polling
+  // setInterval(() => handle.checkExpiry(STORE), STORE.timer.polling);
+} // END JINKIES
+
+// On doc ready, run jinkies.
 $(jinkies);
---
+++ public/js/handle.js
@@ -0,0 +1,33 @@
+/*
+  handle.js
+    will have access to
+      - jQuery
+      - render methods
+      - axios methods
+
+    will
+      - update STATE/STORE
+      - validate input
+      - call render methods
+
+    may
+      - manipulate DOM
+
+  Notes:
+  const state = event.data;
+  const el = $(event.target);
+  calls to api._method_ => axios.js
+*/
+
+'use strict';
+let debug;
+
+const handle = {
+  signup: function(event) {
+    //do stuff
+  },
+
+  create: function(event) {
+    //things
+  }
+};
