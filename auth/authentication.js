86bd2d03649b8b7ce968a39e2f67907c2e44e08f
RC Maples <rcmaples@users.noreply.github.com>
2018-09-29 10:48:32 -0400

    autentication.js

---
+++ auth/authentication.js
@@ -0,0 +1,68 @@
+/*
+authentication.js
+  - uses jswonwebtoken to generate tokens
+  - will import user models
+  - will import config
+*/
+
+'use strict';
+
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const config = require('../config/config');
+
+function generateToken(user) {
+  return jwt.sign(
+    {
+      sub: user.id,
+      username: user.username,
+      expiresIn: config.JWT_EXPIRY
+    },
+    config.JWT_SECRET
+  );
+}
+
+// sign in
+exports.signin = function(req, res, next) {
+  // User should already be authenticated by now
+  res.json({ token: generateToken(req.user) });
+};
+
+// sign up
+exports.signup = function(req, res, next) {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'You must provide a username and password.'
+    });
+  }
+
+  // See if user already exists
+  // If it doesn't make one.
+
+  User.findOne({ username: username }, function(err, existingUser) {
+    if (err) {
+      return next(err);
+    } else if (existingUser) {
+      return res.status(422).json({
+        code: 422,
+        reason: 'ValidationError',
+        message: 'Username is already in use.'
+      });
+    } else {
+      const user = new User({
+        name: name,
+        email: email,
+        username: username,
+        password: password
+      });
+      user.save(err => {
+        return next(err);
+      });
+      res.json({ token: generateToken(user) });
+    }
+  });
+};
