1c32fe28330ec8c93013515061def0f330dca5f4
RC Maples <rcmaples@users.noreply.github.com>
2018-09-29 11:07:39 -0400

    added auth strategies.

---
+++ auth/strategies.js
@@ -0,0 +1,58 @@
+/*
+strategies.js
+   - will use passportjs for auth
+   - will import user model
+   - will import config for jwt options
+*/
+
+'use strict';
+
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+const User = require('../models/user');
+const passport = require('passport');
+const { Strategy: LocalStrategy } = require('passport-local');
+const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
+const { JWT_SECRET } = require('../config/config');
+
+const localStrategy = new LocalStrategy((username, password, done) => {
+  User.findOne({ username: username }, function(err, user) {
+    if (err) {
+      return done(err);
+    }
+    if (!user) {
+      return done(null, false);
+    }
+    user.comparePassword(password, function(err, isMatch) {
+      if (err) {
+        return done(err);
+      }
+      if (!isMatch) {
+        return done(null, false);
+      }
+      return done(null, user);
+    });
+  });
+});
+
+const jwtOptions = {
+  secretOrKey: JWT_SECRET,
+  jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('Berarer'),
+  algorithms: ['HS256']
+};
+
+const jwtStrategy = new JwtStrategy(jwtOptions, (payload, done) => {
+  User.findById(payload.sub, function(err, user) {
+    if (err) {
+      return done(err, false);
+    }
+    if (user) {
+      done(null, user);
+    } else {
+      done(null, false);
+    }
+  });
+});
+
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+module.exports = { localStrategy, jwtStrategy };
