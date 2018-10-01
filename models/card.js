203f57e8d93ec9a848489688ad65058e26ccb12c
RC Maples <rcmaples@users.noreply.github.com>
2018-10-01 08:45:07 -0400

    Board and Card Schemas :white_check_mark:

--- models/card.js
+++ models/card.js
@@ -1,10 +1,48 @@
 /*
-user.js
+card.js
 
-  uses bcryptjs for password encryption
-  uses mongoose for monogodb schemas
-  will export the User mongoose model based on the UserSchema
+cards will contain
+  - text (String, required)
+  - an Owner
+  - completed (boolean)
+  - completed at (timestamp/number)
+  - board, an object id for it's parent board
+
+cards will
+  - user Mongoose for schema
+  - enforce .trim() using mongoose trim
+  - keep track of timestamps
 
 */
 
 'use strict';
+const mongoose = require('mongoose');
+
+const CardSchema = mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 1
+    },
+    completed: { type: Boolean, default: false },
+    completedAt: { type: Number, defualt: null },
+    owner: { type: String },
+    board: { type: Schema.Types.ObjectId, ref: 'Board' }
+  },
+  { timestamps: true }
+);
+
+CardSchema.methods.serialize = function() {
+  return {
+    id: this._id,
+    text: this.text,
+    completed: completed,
+    owner: this.owner,
+    board: this.board._id
+  };
+};
+
+const Card = mongoose.model('Card', CardSchema);
+module.exports = { Card };
